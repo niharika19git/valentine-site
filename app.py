dcfe73a18f3ccaca7739e3f1ec2c644eb4cee15f
niharika19git <niharika.190700@gmail.com>
2026-02-08 23:41:27 -0500

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 from flask import Flask, render_template
 
-# create the website app
 app = Flask(__name__)
 
-# first page (card)
 @app.route("/")
 def card():
     return render_template("card.html")
 
-# second page (gallery)
 @app.route("/gallery")
 def gallery():
     return render_template("gallery.html")
 
-# third page (final heart)
 @app.route("/end")
 def end():
     return render_template("end.html")
 
-# run the website
-app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
